675f6b3d8b0002576e2b0c9862ea29161486d8cb
Templin, Chris <ctemplin@gmail.com>
2023-01-11 18:56:02 -0500

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,53 @@
+import useAsset from "ultra/hooks/use-asset.js";
+import { Link, Route, Switch } from "wouter";
+import HomePage from "./pages/Home.jsx";
+import AboutPage from "./pages/About.jsx";
+
+export default function App() {
+  console.log("Hello world!");
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>Ultra</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href={useAsset("/favicon.ico")} />
+        <link rel="stylesheet" href={useAsset("/style.css")} />
+      </head>
+      <body>
+        <main>
+          <h1>
+            <span></span>__<span></span>
+          </h1>
+          <p>
+            Welcome to{" "}
+            <strong>Ultra</strong>. This is a barebones starter for your web
+            app.
+          </p>
+          <p>
+            Take{" "}
+            <a
+              href="https://ultrajs.dev/docs"
+              target="_blank"
+            >
+              this
+            </a>, you may need it where you are going. It will show you how to
+            customize your routing, data fetching, and styling with popular
+            libraries.
+          </p>
+          <Switch>
+            <Route path="/">
+              <HomePage />
+            </Route>
+            <Route path="/about">
+              <AboutPage />
+            </Route>
+            <Route>
+              404
+            </Route>
+          </Switch>
+        </main>
+      </body>
+    </html>
+  );
+}
